484f5c73d0939344ced84aebdd3fcbdc41e04b88
Anderson Salles <agsalles@gmail.com>
2018-09-08 20:57:09 -0300

    Criacao do projeto e conectando ao mysql 3.2

---
+++ src/main/java/com/devapps/devmoney/DevmoneyApiApplication.java
@@ -0,0 +1,12 @@
+package com.devapps.devmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevmoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevmoneyApiApplication.class, args);
+	}
+}
